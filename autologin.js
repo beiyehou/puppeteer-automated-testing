035f7834e5fd55cf6a368de56b29ca339347f276
kevinluohuan <kevinluohuan@didichuxing.com>
2018-06-19 10:23:25 +0800

    完成对chromium 执行位置的兼容

--- autologin.js
+++ autologin.js
@@ -1,33 +1,51 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 const iPhone6 = devices['iPhone 6'];
 const baiduAccount = require("./config/baidu.json");
-puppeteer.launch({
-    executablePath:'./chrome-mac/Chromium.app/Contents/MacOS/Chromium',
+const chromiumConfig = require("./config/chromium.json");
+
+let launchOptions = {
     headless: false,
     slowMo: 500
-}).then(async browser => {
+};
+
+if (chromiumConfig.executablePath) {
+    launchOptions.executablePath = chromiumConfig.executablePath;
+}
+
+//executablePath:'./chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+
+/** Testing the screenshots folder and create it if not existing. **/
+try {
+    fs.accessSync("./screenshots", fs.constants.F_OK);
+} catch(err) {
+    fs.mkdirSync("./screenshots");
+}
+
+
+puppeteer.launch(launchOptions).then(async browser => {
     const page = await browser.newPage();
     await page.emulate(iPhone6);
     /** Load baidu homepage. **/
     await page.goto('https://www.baidu.com');
     /** Click user icon and redirect to login page. **/
     await page.waitForSelector('#header #login');
-    await page.screenshot({path: './screenshots/baidu/homepage.png', type:'png'});
+    await page.screenshot({path: './screenshots/homepage.png', type:'png'});
     const $loginEle = await page.$('#header #login');
     if ($loginEle) {
         await $loginEle.click();
     }
     /** Click login button. **/
     await page.waitForSelector("#page-bd .user-login a");
-    await page.screenshot({path: './screenshots/baidu/settingpage.png', type:'png'});
+    await page.screenshot({path: './screenshots/settingpage.png', type:'png'});
     const $loginBtn = await page.$("#page-bd .user-login a");
     if ($loginBtn) {
         await $loginBtn.click();
     }
     /** Now login with password and username **/
     await page.waitForSelector("#login-formWrapper");
-    await page.screenshot({path: './screenshots/baidu/loginpage.png', type:'png'});
+    await page.screenshot({path: './screenshots/loginpage.png', type:'png'});
     const $loginForm = await page.$("#login-formWrapper");
     const $userNameInput = await $loginForm.$("#login-username");
     const $passwordInput = await $loginForm.$("#login-password");
@@ -36,6 +54,6 @@ puppeteer.launch({
     const $submitBtn = await $loginForm.$("#login-submit");
     await $submitBtn.click();
     /** Login finished. **/
-    await page.screenshot({path: './screenshots/baidu/loginfinished.png', type:'png'});
+    await page.screenshot({path: './screenshots/loginfinished.png', type:'png'});
     browser.close();
 });
